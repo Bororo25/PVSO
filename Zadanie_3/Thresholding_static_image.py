ce32952521b64d795d60d9cf557695e16a5ffa34
Ivan Vanca <ivkovanca@gmail.com>
2026-04-06 17:14:42 +0200

    Basic treshold pictures

--- Zadanie_3/Thresholding_static_image.py
+++ Zadanie_3/Thresholding_static_image.py
@@ -1,5 +1,6 @@
 import numpy
 import cv2
+import time
 
 # def histogram(img):
 #     hist = []
@@ -13,46 +14,45 @@ import cv2
 #     return hist
 
 
-img1 = cv2.imread("Castle.png")
-img2 = cv2.imread("Car.png")
+img = cv2.imread("Car.png")
+# img2 = cv2.imread("Car.png")
 
-if img1 is None:
+if img is None:
     print('The image 1 is empty')
-if img2 is None:
-    print('The image 2 is empty')
+# if img2 is None:
+#     print('The image 2 is empty')
 
 
-img1 = cv2.resize(img1,(1000,600),interpolation=cv2.INTER_AREA)
-img2 = cv2.resize(img2,(1000,600),interpolation=cv2.INTER_AREA)
-img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+img = cv2.resize(img,(1000,600),interpolation=cv2.INTER_AREA)
+# img2 = cv2.resize(img2,(1000,600),interpolation=cv2.INTER_AREA)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-print(img1.shape)
-print(img2.shape)
+print(img.shape)
+# print(img2.shape)
 #print(f"Prvy pixel {img1[0][0]}")
 
-limit = 127
 
-for i in range(img1.shape[0]):
-    for j in range(img1.shape[1]):
-        if(img1[i][j]>=limit):
-            img1[i][j]=255
-        else:
-            img1[i][j]=0
+# limit = 100
+for limit in range(20,230,10):
+    img1 = img.copy()
+    for i in range(img1.shape[0]):
+        for j in range(img1.shape[1]):
+            if(img1[i][j]>=limit):
+                img1[i][j]=255
+            else:
+                img1[i][j]=0
 
-for i in range(img2.shape[0]):
-    for j in range(img2.shape[1]):
-        if(img2[i][j]>=limit):
-            img2[i][j]=255
-        else:
-            img2[i][j]=0
 
-#cv2.imshow("Castle",img1)
-cv2.imshow("Car",img2)
+
+    cv2.imshow("Castle",img1)
+    cv2.imwrite(f"Car-basic-treshold-t={limit}.png",img1)
+    cv2.waitKey(500)
+# cv2.imshow("Car",img2)
 #hist = histogram(img2)
 #print(hist)
 
 
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
